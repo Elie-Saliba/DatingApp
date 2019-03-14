f322513d0e5df77ff309e6c510354750c8e7eac5
Elie-Saliba <45711153+Elie-Saliba@users.noreply.github.com>
2019-03-14 17:51:34 +0200

    End Of Section 8

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -5,11 +5,10 @@ namespace DatingApp.API.Data
 {
     public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> optiions) : base(optiions){}
-        
+        public DataContext(DbContextOptions<DataContext> optiions) : base(optiions) { }
         public DbSet<Value> Values { get; set; }
-
-        public DbSet<User> Users{ get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Photo> Photo { get; set; }
     }
 
 }
