df952349f662f167b0100f9b1acba62462d72b93
Elie-Saliba <45711153+Elie-Saliba@users.noreply.github.com>
2019-03-15 12:19:10 +0200

    End of section 10

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -8,6 +8,9 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { resolve } from 'q';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 export const appRootes: Routes = [
     { path: '', component: HomeComponent },
@@ -18,6 +21,8 @@ export const appRootes: Routes = [
         children: [
             { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver}  },
             { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver}},
+            { path: 'member/edit', component: MemberEditComponent,
+             resolve: {user: MemberEditResolver}, canDeactivate: [PreventUnsavedChanges] },
             { path: 'messages', component: MessagesComponent },
             { path: 'lists', component: ListsComponent },
         ]
